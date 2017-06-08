c75cdf5ce7a2f3a21f8359e0a578212a86981577
Jocelyn Tuohy <jocelyntuohy@hotmail.com>
2017-06-08 16:34:48 -0700

    completed dojo survey assignment

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 
 # Hint: Although we've told you never to render from a post route, you'll need
 # to do so for this assignment. We'll show you tools to avoid doing so soon.
@@ -6,8 +6,16 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
-def ():
+def index():
     return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    dinosaur = request.form['dinosaur']
+    beatle = request.form['beatle']
+    return render_template('result.html', name=name, dinosaur=dinosaur,
+                           beatle=beatle)
 
 app.run(debug=True)
 # Nothing after the debug line will be read.
