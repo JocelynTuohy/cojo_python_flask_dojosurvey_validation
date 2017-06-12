ecfeb1f128afc14e56262faab5144b7ef9e98a78
Jocelyn Tuohy <jocelyntuohy@hotmail.com>
2017-06-11 21:16:07 -0700

    added basic data validation to name and comment fields

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash
 
 # Hint: Although we've told you never to render from a post route, you'll need
 # to do so for this assignment. We'll show you tools to avoid doing so soon.
 
 app = Flask(__name__)
+app.secret_key = "KeepItSecretButNotTooSecret"
 
 @app.route('/')
 def index():
@@ -11,12 +12,30 @@ def index():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    name = request.form['name']
-    dinosaur = request.form['dinosaur']
-    beatle = request.form['beatle']
-    comment = request.form['comment']
-    return render_template('result.html', name=name, dinosaur=dinosaur,
-                           beatle=beatle, comment=comment)
+    redirectBool = False
+    print len(request.form['name'])
+    if len(request.form['name']) < 1:
+        flash("Name is a required field.")
+        redirectBool = True
+    if len(request.form['comment']) < 1:
+        flash("Comment(s) are required.")
+        redirectBool = True
+    elif len(request.form['comment']) > 120:
+        flash("Comment(s) may not exceed 120 characters.")
+        redirectBool = True
+    if redirectBool:
+        return redirect('/')
+    else:
+        name = request.form['name']
+        dinosaur = request.form['dinosaur']
+        beatle = request.form['beatle']
+        comment = request.form['comment']
+        return render_template(
+            'result.html',
+            name=name,
+            dinosaur=dinosaur,
+            beatle=beatle,
+            comment=comment)
 
 app.run(debug=True)
 # Nothing after the debug line will be read.
